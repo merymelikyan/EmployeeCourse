ef4dc07cd0c4f595996f465289ba3f4c6b8479f3
Mery <mery.melikyan1973@gmail.com>
2024-06-06 11:28:50 +0400

    added new functionality for image upload and editted API then added TEAM to API

--- employees/models.py
+++ employees/models.py
@@ -3,19 +3,23 @@ from django.utils import timezone
 
 
 class Department(models.Model):
-    title=models.CharField(max_length=255)
+    title= models.CharField(max_length=255)
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = "department"
+        verbose_name_plural = "departments"
+
 
 class Employee(models.Model):
-    first_name =models.CharField(max_length=55)
-    last_name =models.CharField(max_length=55)
-    email =models.EmailField(max_length=255)
-    is_married =models.BooleanField(default=False)
-    salary =models.FloatField(default=0.0)
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+    email = models.EmailField(max_length=255)
+    is_married = models.BooleanField(default=False)
+    salary = models.FloatField(default=0.0)
     contract_exp = models.DateTimeField()
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
@@ -23,6 +27,9 @@ class Employee(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name} | {self.department}"
 
+    class Meta:
+        verbose_name = "employee"
+        verbose_name_plural = "employees"
 
 class About(models.Model):
     title = models.CharField(max_length=255)
@@ -32,6 +39,11 @@ class About(models.Model):
         return self.title
 
 
+    class Meta:
+        verbose_name = "about"
+        verbose_name_plural = "abouts"
+
+
 class Contact(models.Model):
     address = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
@@ -39,4 +51,26 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"{self.address} | {self.email} |{self.phone}"
+
+    class Meta:
+        verbose_name = "contact"
+        verbose_name_plural = "contacts"
+
+
+class Team(models.Model):
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+    position = models.CharField(max_length=55)
+    about = models.TextField(max_length=255)
+    avatar = models.ImageField(
+        upload_to = "team")
+    
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} | {self.position}"
+
+    class Meta:
+        verbose_name = "team"
+        verbose_name_plural = "team"
+
 
--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
-from .models import Department, Employee, About, Contact
+from .models import Department, Employee, About, Contact, Team
 
 def index(request):
     return render(request, "home.html")
@@ -13,7 +13,11 @@ def employees(request):
 
 def about(request):
     about_text = About.objects.all()
-    return render(request, "about.html", {"about_text": about_text})
+    team = Team.objects.all()
+    return render(request, "about.html", {
+        "about_text": about_text,
+        "team": team
+        })
 
 
 def contact(request):
@@ -25,3 +29,6 @@ def single_employee(request, employee_id):
     employee = Employee.objects.get(pk=employee_id)
     return render(request, "single_employee.html", {"employee": employee})
 
+def single_member(request, member_id):
+    member = Team.objects.get(pk=member_id)
+    return render(request, "single_member.html", {"member": member})
